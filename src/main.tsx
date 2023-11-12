eebef743ea75ff832dd9906001525f8bd4c11c85
JAYCEE <Jayceexxiii@gmail.com>
2023-11-11 21:15:22 -0800

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,19 @@
+// Using React 18's new root API
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  
-} from 'react-router-dom';
+import ReactDOM from 'react-dom/client'; // Import 'client' from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import './styles/App.scss';
 
 // Other component imports...
 
-ReactDOM.render(
+// Using the non-null assertion operator to assert that the element is not null
+const root = ReactDOM.createRoot(document.getElementById('root')!); // Notice the '!' at the end
+
+root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        {/* Define other routes here */}
-      </Routes>
+      <App /> {/* App is no longer a Route; it’s just a component. */}
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
